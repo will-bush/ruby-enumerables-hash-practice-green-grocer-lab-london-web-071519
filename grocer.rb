a1a35546003913f72333a919debe34efa30525f9
Will Bush <williamdcbush@gmail.com>
2019-07-08 11:24:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,21 @@
+cart = [
+  {"AVOCADO" => {:price => 3.00, :clearance => true }},
+  {"AVOCADO" => {:price => 3.00, :clearance => true }},
+  {"KALE"    => {:price => 3.00, :clearance => false}}
+  ]
+  
 def consolidate_cart(cart)
-  # code here
+  consolidated_cart_hash {}
+  cart.each do
+    |cart_item| 
+    cart_item.each do |name, price|
+      if consolidated_cart_hash[name].nil?
+        consolidated_cart_hash[name] = price.merge({:count => 1})
+      else
+        consolidated_cart_hash[name][:count] += 1
+      end
+  end
+  consolidated_cart_hash
 end
 
 def apply_coupons(cart, coupons)
